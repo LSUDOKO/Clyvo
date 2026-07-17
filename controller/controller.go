a9b0905b6094cd74cb3bbe0cfbdf7e5c4e478ef2
LSUDOKO <adoranto737@gmail.com>
2026-07-17 20:37:44 +0530

    complte base for controller

---
+++ controller/controller.go
@@ -0,0 +1,33 @@
+package controller
+
+import (
+	"gin"
+)
+
+func HashPassword(password string) string {
+
+}
+
+func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
+
+}
+
+func Signup() gin.HandlerFunc {
+
+}
+
+func Login() gin.HandlerFunc {
+
+}
+
+func ProductViewerAdmin() gin.HandlerFunc {
+
+}
+
+func searchProductByQuery() gin.HandlerFunc {
+
+}
+
+func searchProduct() gin.HandlerFunc {
+
+}
