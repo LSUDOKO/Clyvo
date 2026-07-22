9e88c6edc9282705a948a6d6f995810a8551d491
LSUDOKO <adoranto737@gmail.com>
2026-07-23 00:03:02 +0530

    complte database.go

--- controller/controller.go
+++ controller/controller.go
@@ -13,7 +13,9 @@ func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
 }
 
 func Signup() gin.HandlerFunc {
-
+	return func(c *gin.Context){
+		car ctx,cancel=context
+	}
 }
 
 func Login() gin.HandlerFunc {
