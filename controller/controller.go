69a4128904e4fddc3b72815e7e62fd50369113d4
LSUDOKO <adoranto737@gmail.com>
2026-07-23 23:54:46 +0530

    complete-signup-part

--- controller/controller.go
+++ controller/controller.go
@@ -1,7 +1,13 @@
 package controller
 
 import (
+	"context"
 	"gin"
+	"net/http"
+	"time"
+
+	"github.com/LSUDOKO/Clyvo/models"
+	"go.mongodb.org/mongo-driver/bson"
 )
 
 func HashPassword(password string) string {
@@ -13,8 +19,24 @@ func VerifyPassword(userPassword string, givenPassword string) (bool, string) {
 }
 
 func Signup() gin.HandlerFunc {
-	return func(c *gin.Context){
-		car ctx,cancel=context
+	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		var user models.User
+
+		if err := c.BindJSON(&user); err != nil {
+			c.JSON(400, gin.H{"error": err.Error()})
+			return
+		}
+		ValidationErr := Validate.Struct(user)
+		if ValidationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": ValidationErr.Error()})
+			return
+		}
+
+		count, err := UserCollection.CountDocuments(ctx, bson.M{"email": user.Email})
+
 	}
 }
 
